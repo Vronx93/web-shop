2b4e6c2f543c179762dd625d8fc125a2257ad1a3
Vronx93 <92084864+Vronx93@users.noreply.github.com>
2024-06-04 03:20:55 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/web-shop/",
+  base: "/web-shop/",
   plugins: [react()],
   test: {
     globals: true,
